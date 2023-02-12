a571917f35d4bde343845b822d27dccafb4c6443
Josivaldo Silva <josivaldslv81@gmail.com>
2023-02-12 13:27:36 +0000

    tw became main

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './style.css'
+
+const showMenu = document.querySelector('#hamburger')
+const closeMenu = document.querySelector('#close')
+const mobile = document.querySelector('#menu')
+
+const btns = [showMenu, closeMenu]
+
+btns.map((btn) => {
+    btn.addEventListener('click', () => {
+        mobile.classList.toggle('active')
+    })
+})
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
       rollupOptions: {
         input: {
             main: resolve(__dirname, 'index.html'),
-            tw: resolve(__dirname, 'tw/index.html'),
+            // tw: resolve(__dirname, 'tw/index.html'),
           },
       },
     },
